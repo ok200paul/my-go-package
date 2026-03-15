e2cf5e2c48ce391cda1c4efc1128cd6f57315018
Paul Grimes <paul@ok200.net>
2026-03-15 19:17:04 +1100

    Initial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ok200paul/my-go-package
+
+go 1.25.4
+
+require github.com/runlicense/sdk-go v0.0.0-20260315074618-2a1d7c08ecbc // indirect
---
+++ main.go
@@ -0,0 +1,36 @@
+package mygopackage
+
+import (
+    "context"
+    _ "embed"
+    "errors"
+
+    runlicense "github.com/runlicense/sdk-go"
+)
+
+//go:embed keys/runlicense.key
+var publicKey string
+
+var ErrUnlicensed = errors.New("my-go-package: license verification failed")
+
+var licenseErr error
+
+func init() {
+    _, err := runlicense.Activate(context.Background(), "ok200paul/my-go-package", publicKey)
+    if err != nil {
+        licenseErr = ErrUnlicensed
+    }
+}
+
+type Client struct{}
+
+func New() (*Client, error) {
+    if licenseErr != nil {
+        return nil, licenseErr
+    }
+    return &Client{}, nil
+}
+
+func (c *Client) DoSomething() string {
+    return "something done"
+}
